c2eb9c8594d88b59d10516be4f37556aa5c48f8c
ViolinRobot <isaacsiebelink@gmail.com>
2019-04-25 11:35:40 -0700

    4/25/19 11:35 am

--- drone_world/src/reference/MyDroneControllerJISt.java
+++ drone_world/src/reference/MyDroneControllerJISt.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -29,6 +30,8 @@ public class MyDroneControllerJISt extends DroneControllerSkeleton
 	//{
 	//	return "JISt Drone #"+incrementDroneCounter();
 	//}
+	
+	private LinkedList<String> route = new LinkedList<String>();
 
 	@Override
 	public String getCompanyName() { return "JISt";	}
@@ -38,82 +41,87 @@ public class MyDroneControllerJISt extends DroneControllerSkeleton
 	
 	@Override
 	public void droneIdling(Drone drone) 
-	{				
+	{		
 		// Notify the parent class of the current event
 		super.droneIdling(drone);
 
 		// Find out where the drone currently is (idling at it's last destination)
-		Place x = drone.getDestination();
-		drone.getPassengers();
+		Place currentLocation = drone.getDestination();
+		if (currentLocation.getName().equals(route.getFirst()))
+			route.removeFirst();
 
 		// Get all the possible places to go
-		TreeSet<Place> places = getSimulator().getPlaces();
-		places.remove(x);
 		ArrayList<Place> listOfPlaces = new ArrayList<Place>();
-		listOfPlaces.addAll(places);
-		Collections.sort(listOfPlaces);
+		listOfPlaces.addAll(getAllPossiblePlaces(currentLocation));
 
 		// Get all the people riding the drone
-		Set<Person> people = drone.getPassengers();
-		ArrayList<Person> listOfPeople = new ArrayList<Person>();
-		listOfPeople.addAll(people);
+		ArrayList<Person> currentPassengers = new ArrayList<Person>();
+		currentPassengers.addAll(drone.getPassengers());
 
 		// Get all nearby places to go
 		ArrayList<Place> listOfNearbyPlaces = new ArrayList<Place>();
-		for (int j=0; j < listOfPlaces.size(); j++) {
-			double distance = DistanceCalculator.distance(x.getPosition().getLatitude(),x.getPosition().getLongitude(),
-					listOfPlaces.get(j).getPosition().getLatitude(),listOfPlaces.get(j).getPosition().getLongitude());
-			if (distance*drone.getDischargeRate() <= 0.9) {
-				listOfNearbyPlaces.add(listOfPlaces.get(j));
-			}
-		}
+		listOfNearbyPlaces.addAll(getInRangePlaces(currentLocation, drone.getDischargeRate()));
 
 		// Obtain a random number bounded by the size of the nearby-places-list
-		int r = (int)(Math.random() * listOfNearbyPlaces.size() + 1);
+		//int r = (int)(Math.random() * listOfNearbyPlaces.size() + 1);
 
-		// Figure out where people want to go
-		String nextPlace = listOfNearbyPlaces.get(r - 1).getName();
-		Map<String, Integer> locationMap = new HashMap<>();
-			for (Person candidate : x.getWaitingToEmbark()) {
-			Integer val = locationMap.get(candidate.getDestination());
-			locationMap.put(candidate.getDestination(), val == null ? 1 : val + 1);
+		// Figure out where people want to go if there is not already a pre-planned route
+		if (currentPassengers.size() > 0 && route.size() < 1)
+		{
+			
 		}
-
-		Entry<String, Integer> max =null;
-
-		for (Entry<String, Integer> e : locationMap.entrySet()) {
-			if (max == null || e.getValue() > max.getValue()) {
-				max = e;
-			}
-		}
-
-		nextPlace = max.getKey();
-
-		// Get the places with the most ninjas
-		ArrayList<Place> listOfNinjaPlaces = new ArrayList<Place>();
-		for (int j=0; j < listOfNearbyPlaces.size(); j++) {
-			if (listOfNearbyPlaces.get(j).getWaitingToEmbark().size() > 7) {
-				listOfNinjaPlaces.add(listOfNearbyPlaces.get(j));
-			}
-		}
-		
-		// Find the unload place (I would like to make the unload place the most common destination amongst passengers!!!
-		String unloadPlace = listOfPeople.get(0).getDestination();
-		
+		else
+		{
+			route.clear();
+			route.addFirst(goGetSomePassengersForCryingOutLoud(currentLocation, drone.getDischargeRate()));
+		}		
 
 		// Specify the next routed destination
-		Place routePlace = listOfNearbyPlaces.get(r - 1);
-		if (listOfNinjaPlaces.size() > 5) {
-			routePlace = listOfNinjaPlaces.get((int)(Math.random() * listOfNinjaPlaces.size() - 1));
-		}
+		
 
 		// Tell the passengers where the drone is going
-		getSimulator().setDroneManifest(drone, nextPlace);
+		//getSimulator().setDroneManifest(drone, nextPlace);
 
 		// Send the drone to a new location
-		if (listOfPeople.size() > 7)
-			getSimulator().routeDrone(drone, listOfNearbyPlaces.get(r - 1));
-		else 
-			getSimulator().routeDrone(drone, routePlace);
+		getSimulator().routeDrone(drone, route.getFirst());
+	}
+
+	private String goGetSomePassengersForCryingOutLoud(Place currentLocation, double dischargeRate)
+	{
+		// Get in range places
+		ArrayList<Place> inRangePlaces = new ArrayList<Place>();
+		inRangePlaces.addAll(getInRangePlaces(currentLocation, dischargeRate));
+
+		// Return a random location to go get passengers from
+		Random r = getSimulator().getSimulationController().getRandom();
+		Integer nextRandom = r.nextInt(inRangePlaces.size());
+		return inRangePlaces.get(nextRandom).getName();
+	}
+
+	private TreeSet<Place> getAllPossiblePlaces(Place currentLocation)
+	{
+		TreeSet<Place> places = getSimulator().getPlaces();
+		places.remove(currentLocation);
+		return places;
+	}
+
+	private ArrayList<Place> getInRangePlaces(Place currentLocation, double dischargeRate)
+	{
+		ArrayList<Place> listOfPlaces = new ArrayList<Place>();
+		listOfPlaces.addAll(getAllPossiblePlaces(currentLocation));
+
+		for (int j = 0; j < listOfPlaces.size(); j++) 
+		{
+			double distance = DistanceCalculator.distance(
+								currentLocation.getPosition().getLatitude(),
+								currentLocation.getPosition().getLongitude(),		
+								listOfPlaces.get(j).getPosition().getLatitude(),
+								listOfPlaces.get(j).getPosition().getLongitude()
+								);
+			if (distance*dischargeRate < 1)
+				listOfPlaces.add(listOfPlaces.get(j));
+		}
+
+		return listOfPlaces;
 	}
 }

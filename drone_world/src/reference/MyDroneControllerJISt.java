259132416dc73686625fba494cd3398b956d6604
ViolinRobot <isaacsiebelink@gmail.com>
2019-04-25 20:33:26 -0700

    4/25/19 8:33 pm

--- drone_world/src/reference/MyDroneControllerJISt.java
+++ drone_world/src/reference/MyDroneControllerJISt.java
@@ -7,19 +7,13 @@
 package reference;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.Map.Entry;
 
 import simulator.DistanceCalculator;
 import simulator.Drone;
@@ -53,86 +47,128 @@ public class MyDroneControllerJISt extends DroneControllerSkeleton
 		super.droneIdling(drone);
 
 		// Find out where the drone currently is (idling at it's last destination)
-		Place currentLocation = drone.getDestination();
-		if (currentLocation.getName().equals(route.getFirst()))
-			route.removeFirst();
+		Place x = drone.getDestination();
+		drone.getPassengers();
 
-		// Get all the possible places to go
-		ArrayList<Place> listOfPlaces = new ArrayList<Place>();
-		listOfPlaces.addAll(getAllPossiblePlaces(currentLocation));
+		// Get all places
+		TreeSet<Place> places = getSimulator().getPlaces();
+		places.remove(x);
+		LinkedList<Place> listOfPlaces = new LinkedList<Place>();
+		listOfPlaces.addAll(places);
+		Collections.sort(listOfPlaces);
 
-		// Get all the people riding the drone
-		ArrayList<Person> currentPassengers = new ArrayList<Person>();
-		currentPassengers.addAll(drone.getPassengers());
+		// Get all passengers
+		Set<Person> people = drone.getPassengers();
+		LinkedList<Person> listOfPeople = new LinkedList<Person>();
+		listOfPeople.addAll(people);
+		Collections.sort(listOfPeople);
 
 		// Get all nearby places to go
 		ArrayList<Place> listOfNearbyPlaces = new ArrayList<Place>();
-		listOfNearbyPlaces.addAll(getInRangePlaces(drone, currentLocation));
-
-		// Obtain a random number bounded by the size of the nearby-places-list
-		// int r = (int)(Math.random() * listOfNearbyPlaces.size() + 1);
-
-		// Figure out where people want to go if there is not already a pre-planned
-		// route
-		if (currentPassengers.size() > 0 && route.size() < 1) 
-		{
-			// Got passengers but no route. Go get one!
-			Map<String, Integer> whereTheyGo = new HashMap<String, Integer>();
-			for (Person passenger : currentPassengers) 
-			{
-				whereTheyGo.put(passenger.getDestination(), whereTheyGo.get(passenger.getDestination()) == null ? 1
-						: whereTheyGo.get(passenger.getDestination()) + 1);
+		for (int j=0; j < listOfPlaces.size(); j++) {
+			double distance = DistanceCalculator.distance(x.getPosition().getLatitude(),x.getPosition().getLongitude(),
+					listOfPlaces.get(j).getPosition().getLatitude(),listOfPlaces.get(j).getPosition().getLongitude());
+			if (distance*drone.getDischargeRate() <= 0.9) {
+				listOfNearbyPlaces.add(listOfPlaces.get(j));
 			}
+		}
+		listOfNearbyPlaces.remove(x);	
 
+		// Get the places with the most ninjas
+		ArrayList<String> ninjaStringPlaces = new ArrayList<String>();
+		for (int i = 0; i < listOfNearbyPlaces.size(); i++) {
+			if (listOfNearbyPlaces.get(i).getWaitingToEmbark().size() > 7) {
+				ninjaStringPlaces.add(listOfNearbyPlaces.get(i).getName());
+			}
+		}
+
+		// Turn list of passengers' destinations into a list of strings
+		ArrayList<String> passPlaces = new ArrayList<String>();
+		for (int j=0; j<listOfPeople.size(); j++) {
+			passPlaces.add(listOfPeople.get(j).getDestination());
+		}
+
+		passPlaces.remove(x.getName());		
+		passPlaces.retainAll(ninjaStringPlaces);
+
+		// Turn list of nearby places into a set of strings
+		Set<String> nearbyPlaceString = new TreeSet<String>();
+		for (int j=0; j<listOfNearbyPlaces.size(); j++) {
+			nearbyPlaceString.add(listOfNearbyPlaces.get(j).getName());
+		}
+		
+		// Choose a random nearby place
+		String routePlace = listOfNearbyPlaces.get((int)(Math.random() * listOfNearbyPlaces.size() + 1)).getName();
+		
+		// Cycle through the current passengers to see which is the most popular destination among them
+		if (listOfPeople.size() > 0)
+		{
+			Map<String, Integer> whereTheyGo = new HashMap<String, Integer>();
+			for (Person passenger : listOfPeople)
+			{
+				if (whereTheyGo.get(passenger.getDestination()) == null)
+					whereTheyGo.put(passenger.getDestination(),  1);
+				else
+					whereTheyGo.put(passenger.getDestination(), whereTheyGo.get(passenger.getDestination()) + 1);
+			}
+			
 			String theDestination = "";
 			int theMostVotes = -1;
-
-			for (String key : whereTheyGo.keySet()) 
+			
+			for (String key : whereTheyGo.keySet())
 			{
-				if (whereTheyGo.get(key) > theMostVotes) 
+				if (whereTheyGo.get(key) < theMostVotes)
 				{
 					theDestination = key;
 					theMostVotes = whereTheyGo.get(key);
 				}
 			}
-
-			Place thePlace = null;
-			for (Place place : getAllPossiblePlaces(currentLocation))
-			{
-				if (place.getName().equals(theDestination))
-					thePlace = place;
-			}
-			if(thePlace != null)
-				route.addAll(routeTo(drone, currentLocation, thePlace, new ArrayList<String>()));
-			else
-				System.out.println("We have a problem...");
 		}
-		else if (currentPassengers.size() < 1)
+
+		// If the passenger destination list does not have any locations close, route to the random location aforedefined. Otherwise, go to the the most popular nearby destination
+		if (passPlaces.size() < 1)
+			getSimulator().routeDrone(drone, routePlace);
+		else 
+			getSimulator().routeDrone(drone, passPlaces.get(0));
+
+		// Update the manifest for the passengers at the next destination
+		
+
+		// Get all the places the waiting people want to go to
+		Set<String> waitingDestList = new TreeSet<String>();
+		for (int i= 0; i < drone.getDestination().getWaitingToEmbark().size(); i++) 
 		{
-			// Got no passengers. Go get some!
-			route.clear();
-			route.addFirst(goGetSomePassengersForCryingOutLoud(drone, currentLocation));
+			Place theSink = null;
+			
+			for (Place place : listOfNearbyPlaces)
+			{
+				if (place.getName().equals(x.getWaitingToEmbark().get(i).getDestination()))
+					theSink = place;					
+			}
+			
+			if (theSink != null)
+			{
+				if (isInRange(drone, x, theSink))
+					waitingDestList.add(x.getWaitingToEmbark().get(i).getDestination());
+			}
 		}
-		// The final case is that the drone has both a passenger and a route. Just proceed.
 
-		// Set the manifest
-		Set<String> manifest = new HashSet<String>();
-		manifest.addAll(route);
-		getSimulator().setDroneManifest(drone, manifest);
+		waitingDestList.retainAll(getInRangePlaces(drone, drone.getDestination()));
 
-		// Send the drone to the next location on its route
-		getSimulator().routeDrone(drone, route.getFirst());
+		getSimulator().setDroneManifest(drone, waitingDestList);
 	}
 
 	private ArrayList<String> routeTo(Drone drone, Place start, Place sink, ArrayList<String> route) 
 	{
 		ArrayList<Place> nextJump = getInRangePlaces(drone, start);
 
+		/*
 		if(nextJump.contains(sink))
 		{
 			route.add(sink.getName());
 			return route;
 		}
+		*/
 		
 		for (Place place : nextJump)
 		{
@@ -143,11 +179,8 @@ public class MyDroneControllerJISt extends DroneControllerSkeleton
 			else
 				return routeTo(drone, place, sink, route);
 		}
-	}
 
-	private Object generatePath(Drone drone, ArrayList<Place> neighbors, Place sink)
-	{
-
+		return null;
 	}
 
 	private String goGetSomePassengersForCryingOutLoud(Drone drone, Place currentLocation)
@@ -179,21 +212,6 @@ public class MyDroneControllerJISt extends DroneControllerSkeleton
 								);
 
 		if (distance*drone.getDischargeRate() < 1)
-			return true;
-		else
-			return false;
-	}
-
-	private Boolean isInRange(Drone drone, Place start, Place sink, Double currentDroneCharge)
-	{
-		double distance = DistanceCalculator.distance(
-								start.getPosition().getLatitude(),
-								start.getPosition().getLongitude(),		
-								sink.getPosition().getLatitude(),
-								sink.getPosition().getLongitude()
-								);
-
-		if (distance*drone.getDischargeRate() < currentDroneCharge)
 			return true;
 		else
 			return false;
